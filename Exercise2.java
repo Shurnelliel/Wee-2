8954f08c017597afd535833c8fbd25f4aa02b0e7
Shurnelliel <33744041+Shurnelliel@users.noreply.github.com>
2017-12-04 00:24:45 -0700

    Add files via upload

---
+++ Exercise2.java
@@ -0,0 +1,23 @@
+/** Program: Fahrenheit temperature Program
+* File: Exercise2.java
+* Summary: prompt the user to enter a Fahrenheit temperature.
+* Author: Shertonia S. Stanley
+* Date: December 3, 2017 
+**/
+
+public static void main( String args[ ] )
+     { 
+ 
+//prompt the user to enter a Fahrenheit temperature
+          System.out.println("Fahrenheit temperature"); 
+     }
+}  
+
+
+Enter a Fahrenheit temperature: [107]
+107F is equivalent to 41.6667
+Enter a celius temperature: [107]
+100C is equivalent to 224.6F
+public class Exercise2 {
+
+}
